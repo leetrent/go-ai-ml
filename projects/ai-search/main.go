68f99c4e5f97897ac73d11202a0c40fbbdf35f0e
LeeTrent <lee.trent.1@gmail.com>
2025-12-12 11:26:48 -0600

    Working on it

--- projects/ai-search/main.go
+++ projects/ai-search/main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"bufio"
-	"errors"
 	"flag"
 	"fmt"
-	"io"
 	"os"
-	"strings"
 	"time"
 )
 
@@ -19,48 +15,6 @@ const (
 	ASTAR
 	DIJKSTRA
 )
-
-// Point is a simple struct to store XY coordinates of a node.
-type Point struct {
-	Row int
-	Col int
-}
-
-// Wall is the type used to keep track of potential nodes that
-// are walls, and cannot be explored.
-type Wall struct {
-	State Point
-	wall  bool
-}
-
-// Maze is the type for our game. It keeps track of all the information we need to complete the
-// maze, if possible.
-type Maze struct {
-	Height      int
-	Width       int
-	Start       Point
-	Goal        Point
-	Walls       [][]Wall
-	CurrentNode *Node
-	Solution    Solution
-	Explored    []Point
-	Steps       int
-	NumExplored int
-	Debug       bool
-	SearchType  int
-}
-
-type Node struct {
-	index  int
-	State  Point
-	Parent *Node
-	Action string
-}
-
-type Solution struct {
-	Actions []string
-	Cells   []Point
-}
 
 // main is the entry point to our application.
 func main() {
@@ -101,117 +55,4 @@ func main() {
 	}
 
 	fmt.Println("Explored", len(m.Explored), "nodes.")
-}
-
-func solveDFS(m *Maze) {
-	var s DepthFirstSearch
-	s.Game = m
-	fmt.Println("Goal is", s.Game.Goal)
-	s.Solve()
-}
-
-func (g *Maze) inSolution(x Point) bool {
-	for _, step := range g.Solution.Cells {
-		if step.Row == x.Row && step.Col == x.Col {
-			return true
-		}
-	}
-	return false
-}
-
-func (g *Maze) printMaze() {
-	for r, row := range g.Walls {
-		for c, col := range row {
-			if col.wall {
-				fmt.Print("\u2588")
-			} else if g.Start.Row == col.State.Row && g.Start.Col == col.State.Col {
-				fmt.Print("A")
-			} else if g.Goal.Row == col.State.Row && g.Goal.Col == col.State.Col {
-				fmt.Print("B")
-			} else if g.inSolution(Point{r, c}) {
-				fmt.Print("*")
-			} else {
-				fmt.Print(" ")
-			}
-		}
-		fmt.Println()
-	}
-}
-
-func (g *Maze) Load(fileName string) error {
-	f, err := os.Open(fileName)
-	if err != nil {
-		fmt.Printf("Error opening %s: %s\n", fileName, err)
-	}
-	defer f.Close()
-
-	var fileContents []string
-
-	reader := bufio.NewReader(f)
-	for {
-		line, err := reader.ReadString('\n')
-		if err == io.EOF {
-			break
-		} else if err != nil {
-			return errors.New(fmt.Sprintf("Cannot open file %s: %s", fileName, err))
-		}
-		fileContents = append(fileContents, line)
-	}
-
-	foundStart, foundEnd := false, false
-	for _, line := range fileContents {
-		if strings.Contains(line, "A") {
-			foundStart = true
-		}
-		if strings.Contains(line, "B") {
-			foundEnd = true
-		}
-	}
-
-	if !foundStart {
-		return errors.New("starting location not found.")
-	}
-	if !foundEnd {
-		return errors.New("ending location not found.")
-	}
-
-	g.Height = len(fileContents)
-	g.Width = len(fileContents[0])
-
-	var rows [][]Wall
-
-	for i, row := range fileContents {
-		var cols []Wall
-
-		for j, col := range row {
-			curLetter := fmt.Sprintf("%c", col)
-			var wall Wall
-			switch curLetter {
-			case "A":
-				g.Start = Point{Row: i, Col: j}
-				wall.State.Row = i
-				wall.State.Col = j
-				wall.wall = false
-			case "B":
-				g.Goal = Point{Row: i, Col: j}
-				wall.State.Row = i
-				wall.State.Col = j
-				wall.wall = false
-			case " ":
-				wall.State.Row = i
-				wall.State.Col = j
-				wall.wall = false
-			case "#":
-				wall.State.Row = i
-				wall.State.Col = j
-				wall.wall = true
-			default:
-				continue
-			}
-			cols = append(cols, wall)
-		}
-		rows = append(rows, cols)
-	}
-	g.Walls = rows
-	return nil
 }
